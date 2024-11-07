6a818a24d8a6d6b6fe20f31ba6d40bb1d4726b48
aleksandrstatut <alexandr.statut@gail.com>
2024-11-07 12:52:58 +0200

    Init

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from './prisma';
 import { RoleModule } from './module/role/role.module';
 import { UserModule } from './module/users/user.module';
 import { AuthModule } from './module/auth/auth.module';
+import { MyLoggerModule } from './common/logger/my-logger.module';
 
 @Module({
 	imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './module/auth/auth.module';
 			envFilePath: join('.env'),
 		}),
 		PrismaModule,
+		MyLoggerModule,
 		AuthModule,
 		RoleModule,
 		UserModule,
